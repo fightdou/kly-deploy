5c720834e218fcb39c5d0f561c8129def32fb92f
fightdou <1426589337@qq.com>
2023-08-30 15:26:28 +0800

    edit fsd fsd_nginx trochilus-api haproxy policy

--- ansible/roles/post-deploy/files/recovery_docker.sh
+++ ansible/roles/post-deploy/files/recovery_docker.sh
@@ -7,43 +7,35 @@ echo "`date` ------ recover docker containerd check! ---------" | tee -a $log_fi
 for i in {1..10}; do
     sleep 30
     # 检查 containerd 是否异常
-    containerd_status=$(systemctl is-active containerd)
+    containerd_status=$(systemctl is-failed containerd)
 
-    if [ "$containerd_status" != "active" ]; then
+    if [ "$containerd_status" == "failed" ]; then
         echo "`date` ------ containerd is not active. Performing recovery. ---------" | tee -a $log_file
-        cat /var/log/messages | grep 'panic: invalid freelist page'
-        if [ $? == 0 ]; then
-            # 停止 Docker 服务
-            sudo systemctl stop docker
-            # 备份 containerd 数据库
-            sudo cp -r /var/lib/containerd/ /var/lib/containerd_backup-`date '+%Y-%m-%d-%H:%M:%S'`
-            # 清理 containerd 数据库
-            sudo rm -rf /var/lib/containerd/*
-            # 启动 containerd 服务
-            sudo systemctl restart containerd
+        # 停止 Docker 服务
+        sudo systemctl stop docker
+        # 备份 containerd 数据库
+        sudo cp -r /var/lib/containerd/ /var/lib/containerd_backup-`date '+%Y-%m-%d-%H:%M:%S'`
+        # 清理 containerd 数据库
+        sudo rm -rf /var/lib/containerd/*
+        # 启动 containerd 服务
+        sudo systemctl restart containerd
 
-            containerd_status=$(systemctl is-failed containerd)
-            # 如果还是失败，打印错误信息
-            if [ "$containerd_status" == "failed" ]; then
-                echo "`date` ------ containerd 服务重启失败, 打印日志..." | tee -a $log_file
-                # 打印docker服务的日志
-                journalctl -u containerd.service | tee -a $log_file
-                break
-            else
-                echo "`date` ------ containerd 服务重启成功 !" | tee -a $log_file
-            fi
-            sleep 10
-            # 启动 docker 服务
-            sudo systemctl restart docker
-            docker_status=$(systemctl is-active docker)
-            echo "`date` ------ docker status is $docker_status ---------" | tee -a $log_file
-            echo "`date` ------ Recovery completed ---------" | tee -a $log_file
+        containerd_status=$(systemctl is-failed containerd)
+        # 如果还是失败，打印错误信息
+        if [ "$containerd_status" == "failed" ]; then
+            echo "`date` ------ containerd 服务重启失败, 打印日志..." | tee -a $log_file
+            # 打印docker服务的日志
+            journalctl -u containerd.service | tee -a $log_file
+            break
         else
-            systemctl restart docker
-            docker_status=$(systemctl is-active docker)
-            echo "`date` ------ docker status is $docker_status ---------" | tee -a $log_file
-            echo "`date` ------ Recovery completed ---------" | tee -a $log_file
+            echo "`date` ------ containerd 服务重启成功 !" | tee -a $log_file
         fi
+        sleep 10
+        # 启动 docker 服务
+        sudo systemctl restart docker
+        docker_status=$(systemctl is-active docker)
+        echo "`date` ------ docker status is $docker_status ---------" | tee -a $log_file
+        echo "`date` ------ Recovery completed ---------" | tee -a $log_file
     else
         echo "`date` ------ containerd is active. No recovery needed ---------" | tee -a $log_file
     fi
