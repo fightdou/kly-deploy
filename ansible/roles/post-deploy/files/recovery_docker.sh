0e2f7dfc46b77cd3a8c862c673327a95b05f382f
fightdou <1426589337@qq.com>
2023-08-30 15:29:19 +0800

    edit recovery docker service script

--- ansible/roles/post-deploy/files/recovery_docker.sh
+++ ansible/roles/post-deploy/files/recovery_docker.sh
@@ -7,43 +7,35 @@ echo "`date` ------ recover docker containerd check! ---------" | tee -a $log_fi
 for i in {1..10}; do
     sleep 30
     # 检查 containerd 是否异常
-    containerd_status=$(systemctl is-active containerd)
+    containerd_status=$(systemctl is-failed containerd)
 
-    if [ "$containerd_status" != "active" ]; then
+    if [ "$containerd_status" == "failed" ]; then
         echo "`date` ------ containerd is not active. Performing recovery. ---------" | tee -a $log_file
-        cat /var/log/messages | grep 'panic: invalid freelist page'
-        if [ $? == 0 ]; then
-            # 停止 Docker 服务
-            sudo systemctl stop docker
-            # 备份 containerd 数据库
-            sudo cp -r /var/lib/containerd/ /var/lib/containerd_backup-`date '+%Y-%m-%d-%H:%M:%S'`
-            # 清理 containerd 数据库
-            sudo rm -rf /var/lib/containerd/*
-            # 启动 containerd 服务
-            sudo systemctl restart containerd
+        # 停止 Docker 服务
+        sudo systemctl stop docker
+        # 备份 containerd 数据库
+        sudo cp -r /var/lib/containerd/ /var/lib/containerd_backup-`date '+%Y-%m-%d-%H:%M:%S'`
+        # 清理 containerd 数据库
+        sudo rm -rf /var/lib/containerd/*
+        # 启动 containerd 服务
+        sudo systemctl restart containerd
 
-            containerd_status=$(systemctl is-failed containerd)
-            # 如果还是失败，打印错误信息
-            if [ "$containerd_status" == "failed" ]; then
-                echo "`date` ------ containerd 服务重启失败, 打印日志..." | tee -a $log_file
-                # 打印docker服务的日志
-                journalctl -u containerd.service | tee -a $log_file
-                break
-            else
-                echo "`date` ------ containerd 服务重启成功 !" | tee -a $log_file
-            fi
-            sleep 10
-            # 启动 docker 服务
-            sudo systemctl restart docker
-            docker_status=$(systemctl is-active docker)
-            echo "`date` ------ docker status is $docker_status ---------" | tee -a $log_file
-            echo "`date` ------ Recovery completed ---------" | tee -a $log_file
+        containerd_status=$(systemctl is-failed containerd)
+        # 如果还是失败，打印错误信息
+        if [ "$containerd_status" == "failed" ]; then
+            echo "`date` ------ containerd 服务重启失败, 打印日志..." | tee -a $log_file
+            # 打印docker服务的日志
+            journalctl -u containerd.service | tee -a $log_file
+            break
         else
-            systemctl restart docker
-            docker_status=$(systemctl is-active docker)
-            echo "`date` ------ docker status is $docker_status ---------" | tee -a $log_file
-            echo "`date` ------ Recovery completed ---------" | tee -a $log_file
+            echo "`date` ------ containerd 服务重启成功 !" | tee -a $log_file
         fi
+        sleep 10
+        # 启动 docker 服务
+        sudo systemctl restart docker
+        docker_status=$(systemctl is-active docker)
+        echo "`date` ------ docker status is $docker_status ---------" | tee -a $log_file
+        echo "`date` ------ Recovery completed ---------" | tee -a $log_file
     else
         echo "`date` ------ containerd is active. No recovery needed ---------" | tee -a $log_file
     fi
@@ -51,27 +43,23 @@ for i in {1..10}; do
     # 检查 docker 是否异常
     docker_status=$(systemctl is-failed docker)
     if [ "$docker_status" == "failed" ]; then
-        echo "`date` ------ docker is not active. Performing recovery. ---------" | tee -a $log_file
-        log_line=$(cat /var/log/messages | grep 'panic: page 10 already freed' | wc -l)
-        if [ $log_line -gt 0 ]; then
-            echo "`date` ------ docker 文件系统损坏，自动修复 ---------" | tee -a $log_file
-            # 备份local-kv.db 文件
-            cp /var/lib/docker/network/files/local-kv.db /var/lib/docker/network/files/local-kv.db.bak-`date '+%Y-%m-%d-%H:%M:%S'`
-            # 删除源文件
-            rm -f /var/lib/docker/network/files/local-kv.db
+        echo "`date` ------ docker 文件系统损坏，自动修复 ---------" | tee -a $log_file
+        # 备份local-kv.db 文件
+        cp /var/lib/docker/network/files/local-kv.db /var/lib/docker/network/files/local-kv.db.bak-`date '+%Y-%m-%d-%H:%M:%S'`
+        # 删除源文件
+        rm -f /var/lib/docker/network/files/local-kv.db
 
-            systemctl restart docker
-            recovery_status=$(systemctl is-active docker)
-                # 如果还是失败，打印错误信息
-                if [ "$recovery_status" == "failed" ]; then
-                    echo "`date` ------ Docker 服务重启失败, 打印日志..." | tee -a $log_file
-                    # 打印docker服务的日志
-                    journalctl -u docker.service | tee -a $log_file
-                else
-                    echo "`date` ------ Docker 服务重启成功 !" | tee -a $log_file
-                fi
-            echo "`date` ------ Recovery completed ---------" | tee -a $log_file
-        fi
+        systemctl restart docker
+        recovery_status=$(systemctl is-active docker)
+            # 如果还是失败，打印错误信息
+            if [ "$recovery_status" == "failed" ]; then
+                echo "`date` ------ Docker 服务重启失败, 打印日志..." | tee -a $log_file
+                # 打印docker服务的日志
+                journalctl -u docker.service | tee -a $log_file
+            else
+                echo "`date` ------ Docker 服务重启成功 !" | tee -a $log_file
+            fi
+        echo "`date` ------ Recovery completed ---------" | tee -a $log_file
     else
         echo "`date` ------ docker is active. No recovery needed ---------" | tee -a $log_file
         break
